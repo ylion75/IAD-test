3424a27377abe6643affcd00aa5113464d80121a
Yan Lion <ylion75@gmail.com>
2021-07-08 11:12:05 +0200

    ajout validation saisie email valide

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ContactRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ContactRepository::class)
@@ -31,6 +32,9 @@ class Contact
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email."
+     * )
      */
     private $email;
 
